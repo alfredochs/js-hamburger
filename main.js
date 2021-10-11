24fbbbd6d4069acb7df5d20d2a45e7c12bc9c55c
alfredochs <chanta.alfredo@gmail.com>
2021-10-11 18:09:43 +0200

    toggle

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ buttonX.addEventListener("click", function () {
     headerRight.style.display = "none";
 });
 
+
+// const clickZone = document.querySelector("header .fa-bars");
+// const buttonX = document.querySelector("header .close");
+// const headerRight = document.querySelector("header .hamburger-menu");
+
+// clickZone.addEventListener("click", function () {
+//     headerRight.classList.toggle("close");
+//     buttonX.classList.toggle("");
+// });
