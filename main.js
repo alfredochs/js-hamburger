7a71d785ece1f54be1acb43fcd38e6a2e0e88952
alfredochs <chanta.alfredo@gmail.com>
2021-10-12 14:03:30 +0200

    toggle

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-const clickZone = document.querySelector("header .fa-bars");
-const headerRight = document.querySelector("header .hamburger-menu");
-clickZone.addEventListener("click", function () {
-    headerRight.style.display = "block";
-});
-
-const buttonX = document.querySelector("header .close");
-buttonX.addEventListener("click", function () {
-    headerRight.style.display = "none";
-});
-
-
 // const clickZone = document.querySelector("header .fa-bars");
-// const buttonX = document.querySelector("header .close");
 // const headerRight = document.querySelector("header .hamburger-menu");
+// const buttonX = document.querySelector("header .close");
 
 // clickZone.addEventListener("click", function () {
-//     headerRight.classList.toggle("close");
-//     buttonX.classList.toggle("");
+//     headerRight.style.display = "block";
 // });
+
+// buttonX.addEventListener("click", function () {
+//     headerRight.style.display = "none";
+// });
+
+
+const openButton = document.querySelector("header .header-right > a");
+const closeButton = document.querySelector("header .close");
+const showIt = document.querySelector("header .hamburger-menu");
+
+openButton.addEventListener("click", function () {
+    showIt.classList.toggle("active");
+});
+closeButton.addEventListener("click", function () {
+    showIt.classList.toggle("active");
+});
